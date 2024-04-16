d00ea28e6825ef56f7155e8863ac3d44cdd81c85
aronlee5263 <aronlee5263@gmail.com>
2024-04-16 18:16:53 +0900

    Feat : neon  추가

--- src/App.jsx
+++ src/App.jsx
@@ -37,28 +37,40 @@
 // import logo from "./logo.svg";
 import evesangrolang from "./evesangrolang.jpg";
 import artwork from "./artwork.jpg";
+import artwork2 from "./artwork2.jpg";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <div className="left">
-        <div className="left-text1">DIMENSION</div>
-        <div className="left-text2">FASION</div>
-      </div>
-      <div className="middle">
-        {/* <img src={evesangrolang} className="evesangrolang" alt="backPoster" /> */}
-        <img src={artwork} className="artwork" alt="artwork" />
+    <>
+      <div>asdasd</div>
+      <div className="container">
+        <h1>CHA WON PARTY</h1>
 
-        <div className="middle-text">CHA WON PARTY </div>
-        <div className="middle-text2">Saturday 4.20 </div>
-        <div className="middle-text3">pm 7:00 - 10:00 </div>
+        <div className="left">
+          <div className="left-text1">DIMENSION</div>
+          <div className="left-text2">FASION</div>
+        </div>
+        <div className="middle">
+          {/* <img src={evesangrolang} className="evesangrolang" alt="backPoster" /> */}
+          <img src={artwork2} className="artwork2" alt="artwork2" />
+
+          {/* <div className="middle-text">CHA WON PARTY </div> */}
+          <div className="middleLeft-text1"> Socialing </div>
+          <div className="middleLeft-text2"> Team Game </div>
+          <div className="middleLeft-text3"> Rounge Party </div>
+
+          <div className="middleLeft-text4">Saturday 4.20 </div>
+
+          <div className="middle-text2"> pm 7:00 - 10:00</div>
+          <div className="middle-text3"> 신논현 역</div>
+        </div>
+        <div className="right">
+          <div className="right-text1">SOCIAL</div>
+        </div>
       </div>
-      <div className="right">
-        <div className="right-text1">SOCIAL</div>
-      </div>
-    </div>
+    </>
   );
 }
 
